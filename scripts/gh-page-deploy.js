c3e372ac012cf505d5104e882e8592ec69744d98
Jose Murillo <joseluismurillorios@gmail.com>
2020-11-02 11:59:47 -0800

    Updated deploy.js

--- scripts/gh-page-deploy.js
+++ scripts/gh-page-deploy.js
@@ -7,8 +7,8 @@ const fs = require('fs');
     await execa('git', ['checkout', '--orphan', 'gh-pages']);
     console.log('Building...');
     await execa('npm', ['run', 'build']);
-    // Understand if it's dist or build folder
-    const folderName = fs.existsSync('dist') ? 'dist' : 'build';
+    // Understand if it's demo or build folder
+    const folderName = fs.existsSync('demo') ? 'demo' : 'build';
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
     console.log('Pushing to gh-pages...');
