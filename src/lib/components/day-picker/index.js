5c2f0091b08ae5c9925ed0eee1dcdd0019abd3a1
Jose Murillo <joseluismurillorios@gmail.com>
2020-11-04 16:54:48 -0800

    Started day picker

--- src/lib/components/day-picker/index.js
+++ src/lib/components/day-picker/index.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const DayPicker = props => {
+import './style.scss';
+
+const DayPicker = () => {
   return (
-    <div>
-      
+    <div className="day__picker">
+      <div className="day__picker--main">
+        <div className="day__picker--display">
+          <div className="day__picker--display-values">
+            <div className="day__picker--display-year">2020</div>
+            <div className="day__picker--display-current">
+              <div className="day__picker--display-weekday">Tue</div>
+              <div className="day__picker--display-day">14</div>
+              <div className="day__picker--display-month">Nov</div>
+            </div>
+          </div>
+        </div>
+        <div className="day__picker--wrapper"></div>
+      </div>
     </div>
   )
 }
