78ea261f9477c4fce967166ee6b870869b8baf0b
Jose Murillo <joseluismurillorios@gmail.com>
2020-12-12 10:24:48 +0000

    gh-pages

---
+++ precache-manifest.f40b506d85dc48d8d6c901a13dcd2210.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8747f37ba3360f0029826975774edc0",
+    "url": "/react-simple-datetime/index.html"
+  },
+  {
+    "revision": "72a0a240dc98ae57430e",
+    "url": "/react-simple-datetime/static/css/main.911f92cb.css"
+  },
+  {
+    "revision": "72a0a240dc98ae57430e",
+    "url": "/react-simple-datetime/static/js/main.259504a2.js"
+  }
+]);
